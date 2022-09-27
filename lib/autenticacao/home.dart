a9d1ed1de0c840b3b70fa4bbf5d5c6f62084e473
Lucas <l_ulbrecht@ufms.br>
2022-09-27 10:19:28 -0400

    Adcição Botão + para ir a pagina criar presente

--- lib/autenticacao/home.dart
+++ lib/autenticacao/home.dart
@@ -1,3 +1,4 @@
+import 'package:app_lista_presentes/criarPresentes/newPresentPage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:app_lista_presentes/autenticacao/signin.dart';
 import 'package:flutter/material.dart';
@@ -13,18 +14,26 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Sair"),
-          onPressed: () {
-            FirebaseAuth.instance.signOut().then((value) {
-              print("Você saiu da sua conta");
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => SignInScreen()));
-            });
-          },
+        body: Center(
+          child: ElevatedButton(
+            child: Text("Sair"),
+            onPressed: () {
+              FirebaseAuth.instance.signOut().then((value) {
+                print("Você saiu da sua conta");
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SignInScreen()));
+              });
+            },
+          ),
         ),
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add,
+              size: 30.0, color: Color.fromARGB(255, 227, 233, 236)),
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => NewPresentScreen()));
+          },
+          backgroundColor: Color.fromARGB(255, 71, 32, 197),
+        ));
   }
 }
